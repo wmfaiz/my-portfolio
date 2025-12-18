e6d1eb3546ce4cbbb5ecbee25076ed674d489a55
Wan Faiz <w-faiz@okakichi.co.jp>
2025-12-18 13:33:04 +0800

    feat: integrate SpeedInsights and Analytics components in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} font-mono antialiased bg-black text-emerald-100`}
       >
         {children}
+        <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 type SkillLevel = "High" | "Medium" | "Low";
 
